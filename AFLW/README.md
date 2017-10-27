8ca99fafae5713b27ecb26b766dc648731007ae5
Peter Hickey <peter.hickey@gmail.com>
2017-10-27 14:51:57 +1100

    Add README and update Shiny App

---
+++ AFLW/README.md
@@ -0,0 +1,17 @@
+# AFLW
+
+
+## Demo Shiny App
+
+[https://petehaitch.shinyapps.io/AFLW/](https://petehaitch.shinyapps.io/AFLW/)
+
+## Team
+
+- Nikeisha Caruana 
+  - Twitter: [@bluebirdi](https://twitter.com/bluebirdi)
+  - GitHub: [@njcaruana](https://github.com/njcaruana)
+- Samithree Rajapaksha
+  - GitHub [@spsamithree](https://github.com/spsamithree)
+- Peter Hickey
+  - Twitter: [@PeteHaitch](https://twitter.com/PeteHaitch)
+  - GitHub: [@PeteHaitch](https://github.com/PeteHaitch)
--- AFLW/app.R
+++ AFLW/app.R
@@ -28,7 +28,9 @@ ui <- fluidPage(
     selectInput('color', 'Color', choices = c("Club"), selected = "Club"),
     selectInput('statistic', 'Statistic',
                 choices = unique(tidy_players_tab$Statistic),
-                selected = "Kicks_TOT")
+                selected = "Kicks_AVG"),
+    selectInput('stratify', 'Stratify by', c(None = '.', "Club"), selected = "."),
+    textInput('highlight', 'Highlight player')
   ),
   mainPanel(
     plotlyOutput('trendPlot', height = "900px")
@@ -42,7 +44,9 @@ server <- function(input, output) {
     tidy_players_tab %>%
       filter(Statistic == input$statistic) %>%
       arrange(Value) %>%
-      mutate(Player = factor(Player, levels = Player, ordered = TRUE))
+      mutate(highlight = (Player == input$highlight),
+             Player = factor(Player, levels = Player, ordered = TRUE),
+             ave = ave(Value))
   })
 
   output$trendPlot <- renderPlotly({
@@ -50,12 +54,23 @@ server <- function(input, output) {
     # Build graph with ggplot syntax
     p <- ggplot(dataset(),
                 aes_string(x = input$x, y = input$y, color = input$color)) +
-      geom_point() +
       ggtitle(input$statistic) +
       theme(axis.text.x = element_blank(), axis.ticks.x = element_blank()) +
       # scale_colour_manual(values = setNames(team_cols_tab$col,
       #                                       team_cols_tab$Club))
-      scale_color_brewer(palette = "Dark2")
+      scale_color_brewer(palette = "Dark2") +
+      geom_hline(aes(yintercept = ave))
+
+    # if a facet is specified, add it
+    facets <- paste(input$stratify, "~ .")
+    if (facets != ". ~ .") p <- p + facet_wrap(~ Club)
+
+    # if player is specified, highlight her
+    if (input$highlight != "") {
+      p <- p + geom_point(aes(size = input$highlight))
+    } else {
+      p <- p + geom_point()
+    }
 
     # Make a plotly version of the ggplot
     ggplotly(p) %>%
