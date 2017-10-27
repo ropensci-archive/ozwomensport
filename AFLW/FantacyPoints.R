9ca61ee941c9213327468a9bdb05224dc665a2f2
samithree <samithreer@gmail.com>
2017-10-27 15:08:19 +1100

    Change Variable Names

--- AFLW/FantacyPoints.R
+++ AFLW/FantacyPoints.R
@@ -7,16 +7,15 @@ library(plotly)
 players <- read.csv("AFLW/data/players.csv")
 players=data.frame(players)
 
-#Calculating AFL FAntacy Points
+#Calculating AFL Fantasy Points
 for(i in 1:227){
-  players$Fantacy_Point[i] <- 3*Kicks_AVG[i]+2*Handballs_AVG[i]+3*Marks_AVG[i]+4*Tackles_AVG[i]+Frees.For_AVG[i]-3*Frees.Agst_AVG[i]+Hit.outs_AVG[i]+6*Goals_AVG[i]+Behinds_AVG[i]
+  players$Fantasy_Point[i] <- 3*players$Kicks_AVG[i]+2*players$Handballs_AVG[i]+3*players$Marks_AVG[i]+4*players$Tackles_AVG[i]+players$Frees.For_AVG[i]-3*players$Frees.Agst_AVG[i]+players$Hit.outs_AVG[i]+6*players$Goals_AVG[i]+players$Behinds_AVG[i]
 }
 
-
-p <- ggplot(players, aes(FantacyPoints,Player)) +
-  geom_point(aes(size=FantacyPoints,colour=Club))+
+p <- ggplot(players, aes(Fantasy_Point,Player)) +
+  geom_point(aes(size=Fantasy_Point,colour=Club))+
   scale_size_continuous(range = c(0,5))+
-  xlab("AFL Fantacy Point") +
+  xlab("AFL Fantasy Point") +
   theme(legend.position = "none",axis.text.y=element_blank())
 ggplotly(p)
 
